61abd3912b2cef75c80a02c8ee25158b0a7a529a
Chuck333cunning <142151769+Chuck333cunning@users.noreply.github.com>
2023-08-28 11:25:21 +0200

    Add files via upload

---
+++ encodeur_fait_par_moi_meme.py
@@ -0,0 +1,60 @@
+a = input("Phrases : ")
+if 'a' in a:
+    b = a.replace("a", "2")
+    print(b)
+if 'b' in a:
+    b = a.replace("b", "3")
+if 'c' in a:
+    b = a.replace("c", "4")
+if 'd' in a:
+    b = a.replace("d", "5")
+if 'e' in a:
+    b = a.replace("e", "6")
+if 'f' in a:
+    b = a.replace("f", "7")
+if 'g' in a:
+    b = a.replace("g", "8")
+if 'h' in a:
+    b = a.replace("h", "9")
+if 'i' in a:
+    b = a.replace("i", "10")
+if 'j' in a:
+    b = a.replace("j", "11")
+if 'k' in a:
+    b = a.replace("k", "12")
+if 'l' in a:
+    b = a.replace("l", "13")
+if 'm' in a:
+    b = a.replace("m", "14")
+if 'n' in a:
+    b = a.replace("n", "15")
+if 'o' in a:
+    b = a.replace("o", "16")
+if 'p' in a:
+    b = a.replace("p", "17")
+if 'q' in a:
+    b = a.replace("q", "18")
+if 'r' in a:
+    b = a.replace("r", "19")
+if 's' in a:
+    b = a.replace("s", "20")
+if 't' in a:
+    b = a.replace("t", "21")
+if 'u' in a:
+    b = a.replace("u", "22")
+if 'v' in a:
+    b = a.replace("v", "23")
+if 'w' in a:
+    b = a.replace("w", "24")
+if 'x' in a:
+    b = a.replace("x", "25")
+if 'y' in a:
+    b = a.replace("y", "26")
+if 'z' in a:
+    b = a.replace("z", "1")
+print(b)
+    
+    
+    
+    
+   
